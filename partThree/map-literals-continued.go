241369adc0478a7129c87c881989c96e10cb8e9e
akez <1791189210@qq.com>
2021-01-22 18:20:31 +0800

    Initial commit

---
+++ partThree/map-literals-continued.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type VertexM struct {
+	Lat, Long float64
+}
+/*
+
+映射的文法（续）
+若顶级类型只是一个类型名，你可以在文法的元素中省略它。
+*/
+var map1 = map[string]VertexM{
+	"Bell Labs": {40.68433, -74.39967},
+	"Google": {37.42202, -122.08408},
+}
+
+func main() {
+	fmt.Println(map1)
+}
